dee6b4687e97bc9055791a63b41d6fdc24c89fbd
Alfalahy_Shyma <alfalahyshyma@gmail.com>
2020-10-29 09:44:59 +0300

    backend of details page and dashboard offer

---
+++ controllers/product_detailsController.php
@@ -0,0 +1,35 @@
+<?PHP
+use coreAppNS\Controller;
+//include("app/models/category.php");
+class product_detailsController extends Controller{
+public $controller;
+
+
+
+    function __construct(){
+
+        $this->controller=new Controller();
+        $cat_model=$this->controller->model_object->create_model('product');
+        $id=$_GET['id'];
+        $homeItems=array(
+            'products'=>$cat_model->getDataWhereOrderProd_details($id),
+            'categories'=>$cat_model->getDataWhereOrder(),
+        );
+            //'image'=>$cat_model->getData1(),
+            //'products'=>array('Lenovo','Galaxy s10','Dell Inspiron','Toshiba','HTC'),
+
+            //'products'=>$cat_model->getData1(),
+        
+       
+
+        //$this->controller->view_object->create_view('home',$homeItems);
+        $this->controller->view_object->create_view('product_details',$homeItems);
+       // $this->controller->view_object->create_view('admin/categories',$homeItems);
+    
+       }
+
+
+}
+
+
+?>
---
+++ views/product_details.php
@@ -0,0 +1,127 @@
+<?php include'header.php';?>
+ </br></br>
+ <link rel="stylesheet" href="app/assets/css/jquery.exzoom.css"> 
+ <link rel="stylesheet" href="app/assets/css/styleProduct_de.css">
+ <link rel="stylesheet" href="app/assets/css/zoom.css">
+ <script src="app/assets/js/jquery.exzoom.js"></script> 
+ <script src="app/assets/js/script.js"></script> 
+
+
+
+
+ <body>
+
+		<!-- ggggggggggggggggggggggggggggg -->
+		<div class="container ">
+	        <div class="heading-section">
+	            <h2>تفاصيل المنتج</h2>
+			</div>
+			<?php
+				 $row=$data['products'];
+				 foreach($row as $rows){
+					 
+                    
+                        
+				  
+                  
+							echo'
+							<div class="row mb-4">
+								<div class="col-md-2">
+							  <a href="#"><img src="" class="img-thumbnail p-0 border-0" /></a>
+							</div>
+								<div class="col-md-6">
+									<div class="input-group input-group-lg">
+										
+										
+										
+									</div>
+								</div>
+								<div class="col-md-1">
+									
+								</div>
+								<div class="col-md-1">
+									
+								</div>
+								<div class="col-md-2">
+									
+										</div>
+									</div>
+									<br>
+							<div class="row mt-4">
+								<div class="col-md-6 col-sm-6">
+									<div class="exzoom" id="exzoom">
+										<!-- Images -->
+										<div class="exzoom_img_box" style="background:white">
+										  <ul class="exzoom_img_ul">
+											<li><img src="'.$rows->product_main_image.'"/></li>
+											<li><img src="'.$rows->product_main_image.'"/></li>
+											<li><img src="'.$rows->product_main_image.'"/></li>
+											<li><img src="'.$rows->product_main_image.'"/></li>
+											<li><img src="'.$rows->product_main_image.'"/></li>
+											...
+										  </ul>
+										</div>
+										<!-- Thumbnail Nav-->
+										<div class="exzoom_nav"></div>
+									   
+									  </div>
+									 
+							
+								</div>
+								<div class="col-md-6 col-sm-6">
+									<div class="product-dtl">
+										<div class="product-info">
+											<div class="product-name"> '.$rows->product_name.'</div>
+											
+											<div class="product-price-discount"><span>'.$rows->product_price.'</span><span class="line-through">$29.00</span></div>
+										</div>
+										<p>'.$rows->product_details.'</p>
+										
+										<div class="product-count">
+											<label for="size">Quantity</label>
+											<form action="#" class="display-flex">
+												<div class="qtyminus">-</div>
+												<input type="text" name="quantity" value="1" class="qty">
+												<div class="qtyplus">+</div>
+											</form>
+											<a href="cart?id='.$rows->Product_id.'" class="round-black-btn">Add to Cart</a>
+											
+										</div>
+									</div>
+								</div>
+							</div>';}
+							 ?>
+			
+		</div>
+	</div>
+</div>	
+			<div class="container ">
+	       
+	        <div class="row">
+	        	<div class="col-md-12">
+					<section>
+					<br><br><br><hr>
+			
+		
+		<!-- <div style="text-align:center;font-size:14px;padding-bottom:20px;">Get free icon packs for your next project at <a href="http://iiicons.in/" target="_blank" style="color:#ff5e63;font-weight:bold;">www.iiicons.in</a></div> -->
+	</section>
+	</div>
+		</div>
+		</div>
+	
+	
+	<script type="text/javascript">
+	 $(function(){
+ 
+	   $("#exzoom").exzoom({
+		 // options here
+	   });
+ 
+	 });
+	</script>
+	<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+
+<?php include'footer.php';?>
